09e6d72e737b75f98692a157c8d65ecd32a10520
ajmalpoovanath <ajmal.poovanath@gmail.com>
2024-07-21 19:27:52 +0530

    Initial Commit

---
+++ WRITEPOST/WRITEPOSTApp.swift
@@ -0,0 +1,17 @@
+//
+//  WRITEPOSTApp.swift
+//  WRITEPOST
+//
+//  Created by Ajmal p on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WRITEPOSTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
